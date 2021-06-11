4516d35413e9165224d9a3fc840f3fbe91b85998
xxria17 <xxrcy32@gmail.com>
2021-06-11 21:24:45 +0900

    user 데이터 모델 serializedName 삭제

--- UntilWhenAOS/app/src/main/java/com/dhxxn/untilwhenaos/model/User.kt
+++ UntilWhenAOS/app/src/main/java/com/dhxxn/untilwhenaos/model/User.kt
@@ -1,9 +1,6 @@
 package com.dhxxn.untilwhenaos.model
 
-import com.google.gson.annotations.SerializedName
-
 data class User(
-    @SerializedName("login")
     var id : Int = 0,
     var name : String? = "",
     var password : String? = ""
