afe030cc92130ab13a286aad1225a5fde2c82056
xxria17 <xxrcy32@gmail.com>
2021-06-11 22:44:12 +0900

    user id 데이터 타입 변경

--- UntilWhenServer/src/main/java/com/dhxxn/untilwhen/model/User.java
+++ UntilWhenServer/src/main/java/com/dhxxn/untilwhen/model/User.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     @Column(nullable = false, unique = true)
     private String name;
     @Column(nullable = false)
--- UntilWhenServer/src/main/java/com/dhxxn/untilwhen/repository/UserRepository.java
+++ UntilWhenServer/src/main/java/com/dhxxn/untilwhen/repository/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByName(String name);
 }
